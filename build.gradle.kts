d8e94b4b8a30b2cfe72715cefffe7cdcfbbcc964
Mical <micalhl@sina.cn>
2021-08-17 01:02:17 +0800

    [2.4-pre2] Fix CI build error.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
     
 group = "me.arasple.mc.trhologram"
-version = "2.4-pre1"
+version = "2.4-pre2"
 description = "Modern & Advanced Hologram-Plugin for Minecraft Servers"
 
 taboolib {
