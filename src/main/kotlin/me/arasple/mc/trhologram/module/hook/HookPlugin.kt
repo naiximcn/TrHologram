c2863d5537847b0fe70e44591b26dd26560e1e86
Mical <micalhl@sina.cn>
2021-08-18 13:15:03 +0800

    [2.4-pre8] Dependency Hooked Message.

--- src/main/kotlin/me/arasple/mc/trhologram/module/hook/HookPlugin.kt
+++ src/main/kotlin/me/arasple/mc/trhologram/module/hook/HookPlugin.kt
@@ -1,6 +1,10 @@
 package me.arasple.mc.trhologram.module.hook
 
 import org.bukkit.Bukkit
+import taboolib.common.LifeCycle
+import taboolib.common.platform.Awake
+import taboolib.common.platform.function.console
+import taboolib.module.lang.sendLang
 
 /**
  * @author Arasple
@@ -14,6 +18,13 @@ object HookPlugin {
 
         plugin != null && plugin.isEnabled && plugin.description.version.startsWith("3")
 
+    }
+
+    @Awake(LifeCycle.ENABLE)
+    fun printInfo() {
+        if (TRMENU) {
+            console().sendLang("Plugin-Dependency-Hooked", "TrMenu")
+        }
     }
 
 }
